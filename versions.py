9edeea129411b1786d6eb7f16d3e9792debc1e30
Almothana <almothana12@gmail.com>
2020-09-16 20:28:56 +0000

    added functions

--- versions.py
+++ versions.py
@@ -1,22 +1,27 @@
 import re
 import logging
 import requests
+from packaging.version import parse as parse_version
 from colorama import init, Fore, Back, Style
 
 init() # Initialise Colorama
 
-# The oldest supported versions
-obsolete_apache = "2.4"  # https://www.rapid7.com/db/vulnerabilities/apache-httpd-obsolete
-obsolete_lighttpd = "1.4"  # https://www.rapid7.com/db/vulnerabilities/http-lighttpd-obsolete
-obsolete_php = "7.2"  # https://www.php.net/supported-versions.php
-obsolete_nginx = "1.18" # https://nginx.org/en/download.html
-obsolete_iis = 10
-
+# A dict containing the lastest supported version of each program
+versions = {
+    "Apache": parse_version("2.4"),   # https://www.rapid7.com/db/vulnerabilities/apache-httpd-obsolete
+    "PHP": parse_version("7.2"),      # https://www.php.net/supported-versions.php
+    "Lighttp": parse_version("1.4"),  # https://www.rapid7.com/db/vulnerabilities/http-lighttpd-obsolete
+    "NginX": parse_version("1.18"),   # https://nginx.org/en/download.html
+    "IIS": parse_version("10")
+}
 # response = requests.get("http://dvwa-win10")
 # response = requests.get("http://dvwa-ubuntu")
-response = requests.get("http://192.168.100.66")
+response = requests.get("http://centos82")
+# response = requests.get("http://bee-box")
+# response = requests.get("http://windows7")
+# response = requests.get("http://www.insecurelabs.org/Task/Rule1")
 
-print(response.headers)
+# print(response.status_code)
 
 headers = []
 if 'server' in response.headers:
@@ -35,58 +40,28 @@ if not headers:
     print("could not get server version info")
     exit # TODO
 
-apache_version = ""
-for header in headers:
-    match = re.search(r"apache(\S*)", header.lower())
-    if match:
-        apache_version = re.search(r"\d+\.\d+(\.\d+)*", match.group(1)).group()
-        break
 
-php_version = ""
-for header in headers:
-    match = re.search(r"php(\S*)", header.lower())
-    if match:
-        php_version = re.search(r"\d+\.\d+(\.\d+)*", match.group(1)).group()
-        break
+def get_version(name):
+    """
+        TODO
+    """
+    for header in headers:
+        match = re.search(name.lower() + r"(\S*)", header.lower())
+        if match:
+            version = re.search(r"\d+\.\d+(\.\d+)*", match.group(1))
+            if version:
+                return version.group()
+        return ""
 
-nginx_version = ""
-for header in headers:
-    match = re.search(r"nginx(\S*)", header.lower())
-    if match:
-        nginx_version = re.search(r"\d+\.\d+(\.\d+)*", match.group(1)).group()
-        break
+for name, supported_version in versions.items():
+    server_version = get_version(name)
+    if server_version:
+        server_version = parse_version(server_version)
+        print(f"{name} version: {server_version}", end='')
+        if server_version < supported_version:
+            print(f"{Fore.RED}  (outdated){Style.RESET_ALL}")
+        else:
+            print(f"{Fore.GREEN}  (still supported){Style.RESET_ALL}")
 
-iis_version = ""
-for header in headers:
-    match = re.search(r"iis(\S*)", header.lower())
-    if match:
-        iis_version = re.search(r"\d+\.\d+(\.\d+)*", match.group(1)).group()
-        break
 
-if apache_version:
-    print(f"Apache version: {apache_version}", end='')
-    if apache_version < obsolete_apache:
-        print(f"{Fore.RED}  (obsolete){Style.RESET_ALL}")
-    else:
-        print(f"{Fore.GREEN}  (still supported){Style.RESET_ALL}")
 
-if php_version:
-    print(f"PHP version: {php_version}", end='')
-    if php_version < obsolete_php:
-        print(f"{Fore.RED}  (obsolete){Style.RESET_ALL}")
-    else:
-        print(f"{Fore.GREEN} (still supported){Style.RESET_ALL}")
-
-if nginx_version:
-    print(f"NginX version: {nginx_version}", end='')
-    if nginx_version < obsolete_nginx:
-        print(f"{Fore.RED}  (outdated){Style.RESET_ALL}")
-    else:
-        print(f"{Fore.GREEN} (still supported){Style.RESET_ALL}")
-
-if iis_version:
-    print(f"Microsoft IIS version: {iis_version}", end='')
-    if float(iis_version) < obsolete_iis:
-        print(f"{Fore.RED}  (outdated){Style.RESET_ALL}")
-    else:
-        print(f"{Fore.GREEN} (still supported){Style.RESET_ALL}")
