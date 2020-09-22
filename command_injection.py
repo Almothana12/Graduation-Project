d4bda986882eff920186ba38808a07a0b7304b22
Almothana <almothana12@gmail.com>
2020-09-22 18:15:55 +0000

    Command Injection detection complete

--- command_injection.py
+++ command_injection.py
@@ -9,21 +9,18 @@ import logformatter
 
 log = logging.getLogger(__name__)
 
-# def is_vulnerable(response: requests.Response) -> bool:
-#     """Check if the content of the `response` has an SQL error
+def is_vulnerable(response: requests.Response) -> bool:
+    """Check if the the echo command executed in the server
 
-#     Args:
-#         response (requests.Response): A response object
+    Args:
+        response (requests.Response): A response object
 
-#     Returns:
-#         bool: True if an error is found in the content of `response`, False otherwise
-#     """
-#     with open("SQLIErrors") as SQLIErrors:
-#         for error in SQLIErrors:
-#             error = error.replace("\n", "")
-#             if error.lower() in response.text.lower():
-#                 return True
-#     return False
+    Returns:
+        bool: True if an error is found in the content of `response`, False otherwise
+    """
+    if "echo" not in response.text and "TEST" in response.text:
+        return True
+    return False
 
 
 def time_based(session: requests.Session, url: str, time=10) -> bool:
@@ -34,9 +31,8 @@ def time_based(session: requests.Session, url: str, time=10) -> bool:
         url (str): The URL of the page
 
     Returns:
-        bool: True if SQLi detected, False otherwise
+        bool: True if Command Injection detected, False otherwise
     """
-    time = 10
     t1 = session.get(url).elapsed.total_seconds()
     t2 = session.get(url).elapsed.total_seconds()
     t3 = session.get(url).elapsed.total_seconds()
@@ -74,28 +70,30 @@ def check(session: requests.Session, url: str) -> bool:
     Returns:
         bool: True if Command Injection detected, False otherwise
     """
-    # vulnerable = False
-    # if vulnerable:
-    #     return True
-    # # forms = HTMLParser.get_all_forms(session, url)
-    # # for form in forms:
-    # #     form_details = HTMLParser.get_form_details(form)
-    # #     with open("SQLPayloads") as payloads:
-    # #         for payload in payloads:
-    # #             payload = payload.replace("\n", "")  # remove newline char
-    # #             # print(f"Testing: {payload}")
-    # #             response = HTMLParser.submit_form(
-    # #                 form_details, url, payload, session)
-    # #             if is_vulnerable(response):
-    # #                 log.warning(f"SQLi Detected on {response.url}")
-    # #                 log.info(f"Payload: {payload}")
-
-    # #                 return True
+    vulnerable = False
+    forms = HTMLParser.get_all_forms(session, url)
+    for form in forms:
+        form_details = HTMLParser.get_form_details(form)
+        with open("payloads/CommandInjectionPayloads") as payloads:
+            for payload in payloads:
+                if payload.startswith('#'):  # Comment
+                    continue
+                payload = payload.replace("\n", "")  # remove newline char
+                log.debug(f"ci.time_based: Testing: {payload}")
+                response = HTMLParser.submit_form(
+                    form_details, url, payload, session)
+                if is_vulnerable(response):
+                    log.warning(f"Command Injection found on {response.url}")
+                    log.info(f"Payload: {payload}")
+                    vulnerable = True
+                    break
+    return vulnerable
 
 
 if __name__ == "__main__":
     session = requests.Session()
     session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
-    session.headers["Cookie"] = "security_level=0; PHPSESSID=728c89ca46666766646dcaefbf89e888; has_js=1"
-    url = "http://bee-box/bWAPP/commandi_blind.php"
-    time_based(session, url)
+    session.headers["Cookie"] = "PHPSESSID=2r5bfcokovgu1hjf1v08amcd1g; security=low"
+    # url = "http://bee-box/bWAPP/commandi_blind.php"
+    url = "http://dvwa-win10/vulnerabilities/exec/"
+    check(session, url)
