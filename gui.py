bdca13a229b30d701140b8f43ec362addb5a7428
Almothana <almothana12@gmail.com>
2020-10-21 16:46:12 +0000

    Implemented GUI

---
+++ gui.py
@@ -0,0 +1,99 @@
+import sys
+
+import requests
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
+
+import command_injection
+import data
+import sqli
+import versions
+import xss
+from crawler import get_all_links
+from ui_form import Ui_MainWindow
+
+session = requests.Session()
+session.headers['Cookie'] = "PHPSESSID=2r5bfcokovgu1hjf1v08amcd1g; security=low"
+
+class MainWindow(qtw.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.scanButton.clicked.connect(self.scan)
+
+    def scan(self):
+        print("Dasd")
+        url = self.urlLineEdit.text()
+        cookie = self.cookieLineEdit.text()
+        check_xss = self.xssCheckBox.isChecked()
+        check_sqli = self.sqliCheckBox.isChecked()
+        check_ci = self.ciCheckBox.isChecked()
+        check_version = self.versionCheckBox.isChecked()
+        check_data = self.dataCheckBox.isChecked()
+        if url:
+            if not url.startswith("http"):
+                url = "http://" + url
+        if cookie:
+            session.headers['Cookie'] = cookie
+
+        # if not valid_url(url):
+        #     return
+
+        # if args['--crawl']:
+        #     crawl(url, args)
+        #     return
+        if check_version:
+            versions.check(session, url)
+        if check_data:
+            data.check(session, url)
+        if check_sqli:
+            sqli.check(session, url)
+            # if not args['--no-time-based']:
+            #     sqli.time_based(session, url)
+        if check_xss:
+            # dom = not args['--no-dom']
+            # cookie = args['--cookie']
+            xss.check(session, url, True, cookie)
+        if check_ci:
+            vulnerable = command_injection.check(session, url)
+            if not vulnerable:
+                command_injection.time_based(session, url)
+
+        self.show_popup()
+
+        # print(url)
+        # print(cookie)
+        # print(xss)
+        # self.textBrowser.
+        # self.textBrowser.setText("url: " + url)
+        # self.textBrowser.setText("cookie: " + cookie)
+        # self.textBrowser.setText("xss: " + str(xss))
+        # self.textBrowser.setText("sqli: " + str(sqli))
+        # self.textBrowser.setText("ci: " + str(ci))
+        # self.textBrowser.setText("version: " + str(version))
+        # self.textBrowser.setText("data: " + str(data))
+    def show_popup(self):
+        msg = qtw.QMessageBox()
+        msg.setWindowTitle("Scan Complete")
+        msg.setText("Scan Complete")
+        msg.setIcon(qtw.QMessageBox.Information)
+        msg.setStandardButtons(qtw.QMessageBox.Ok)
+        msg.exec_()
+
+
+def run():
+    app = qtw.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+    # import sys
+    # app = QtWidgets.QApplication(sys.argv)
+    # MainWindow = QtWidgets.QMainWindow()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(MainWindow)
+    # MainWindow.show()
+    # sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run()
--- main.py
+++ main.py
@@ -9,17 +9,17 @@ Usage:
 Options:
     -c, --cookie=<cookie>          Send a Cookie with the requests
     -C, --command-injection        Check for command injection
-        --crawl                    Check for all URLs 
+        --crawl                    Check for all URLs
     -D, --data                     Check for sensitve data
         --gui                      Start a graphical interface
     -h, --help                     Show this screen
         --no-dom                   Don't check for DOM-based XSS
     -S, --sqli                     Check for SQLi
+        --no-time-based            Don't Check using time-based method.
     -v, --version                  Show the version
     -V, --versions                 Check for the server version
     -X, --xss                      Check for XSS
 """
-
 import logging
 import sys
 import requests
@@ -32,24 +32,27 @@ from crawler import get_all_links
 import versions
 from docopt import docopt
 import xss
+import gui
 
 session = requests.Session()
 # session.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
 session.headers['Cookie'] = "PHPSESSID=2r5bfcokovgu1hjf1v08amcd1g; security=low"
-# session.headers["Cache-Control"] = "no-cache"
-# session.headers["Pragma"] = "no-cache"
 
 
 def main():
-    url = "http://dvwa-win10/vulnerabilities/xss_r/"
+    # url = "http://dvwa-win10/vulnerabilities/xss_r/"
     args = docopt(usage)
     # print(args)
     
     if args['--gui'] or all(not x for x in args.values()):
-        pass  # TODO GUI
-        # return
+        print("Launching GUI...")  # TODO GUI
+        gui.run()
+        return
     if args['<url>']:
-        url = args['<url>']
+        if not args['<url>'].startswith("http"):
+            url = "http://" + args['<url>']
+        else:
+            url = args['<url>']
     if args['--cookie']:
         session.headers['Cookie'] = args['--cookie']
 
@@ -67,16 +70,19 @@ def main():
         data.check(session, url)
         d = True
     if args['--sqli']:
-        sqli.check(session, url)
-        d = False
+        vulnerable = sqli.check(session, url)
+        if not args['--no-time-based'] and not vulnerable:
+            sqli.time_based(session, url)
+        d = True
     if args['--xss']:
         dom = not args['--no-dom']
         cookie = args['--cookie']
         xss.check(session, url, dom, cookie)
         d = True
     if args['--command-injection']:
-        command_injection.check(session, url)
-        command_injection.time_based(session, url)
+        vulnerable = command_injection.check(session, url)
+        if not vulnerable:
+            command_injection.time_based(session, url)
         d = True
     # If user didn't specify a vlunerability, check for all vulnerabilities
     if not d:
@@ -84,14 +90,16 @@ def main():
         versions.check(session, url)
 
         data.check(session, url)
+
         sqli.check(session, url)
 
         dom = not args['--no-dom']
         cookie = args['--cookie']
         xss.check(session, url, dom, cookie)
 
-        command_injection.check(session, url)
-        command_injection.time_based(session, url)
+        vulnerable = command_injection.check(session, url)
+        if not vulnerable:
+            command_injection.time_based(session, url)
 
 
 def crawl(url, args):
@@ -120,7 +128,8 @@ def crawl(url, args):
         if not d:
             data.check(session, url)
 
-            sqli.check(session, url)
+            # sqli.check(session, url)
+            sqli.time_based(session, url)
 
             dom = not args['--no-dom']
             cookie = args['--cookie']
@@ -140,10 +149,11 @@ def valid_url(url):
         bool: True if the `url` is valid
     """
     try:
-        r = session.get(url)
-        if r.history:
-            logging.warning(f"Redirected to {r.url}")
-        r.raise_for_status()
+        response = session.get(url)
+        if response.history:
+            logging.warning(f"Redirected to {response.url}")
+        # raise an HTTPError if the request is unsuccessful
+        response.raise_for_status() 
     except requests.ConnectionError as err:
         logging.debug(err)
         logging.error(f"Could Not Reach URL: {err.request.url}")
